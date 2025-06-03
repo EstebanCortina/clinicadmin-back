0b65d6e64db59195ba4e88f61c4baf45981ee11f
Esteban Cortina <ecortina@devteam.hmhsistemas.com.mx>
2025-06-03 04:43:02 -0600

    Add upload & download

--- routes/v1/patient_files.js
+++ routes/v1/patient_files.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import {response, getDb } from "../../helpers/index.js"
 import upload from "../../handlers/multer_handler.js";
-import { addPatientFile } from "../../services/patient_files_services.js";
+import { addPatientFile, getMainPatientFile } from "../../services/patient_files_services.js";
 
 const patientFilesRouter = express.Router({ mergeParams: true })
 
@@ -14,24 +14,50 @@ patientFilesRouter.post("/", upload.single('file'), async (req, res) => {
       return res.status(400).json({ message: 'No se subió ningún archivo' });
     }
 
-    const base64 = file.buffer.toString('base64');
     await addPatientFile(
-        req.params.id,
-        base64,
-        getDb()
-    )
+      req.params.id,
+      file.buffer,
+      getDb()
+    );
 
-    return response(res, 201, {
-      message: "Patient's file uploaded successfully",
+    return res.status(201).json({
+      message: "Archivo del paciente subido exitosamente",
       data: null
     });
   } catch (err) {
     console.error(err);
-    return response(res, 500, {
-      message: "Error uploading patient's file",
+    return res.status(500).json({
+      message: "Error al subir el archivo del paciente",
       data: null
     });
   }
 });
+
+
+patientFilesRouter.get("/main", async (req, res) => {
+  try {
+    const db = getDb();
+    const { file } = await getMainPatientFile(req.params.id, db);
+
+    if (!file || file.length === 0) {
+      return res.status(404).json({ message: "Archivo no encontrado" });
+    }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="patient_file_${req.params.id}.pdf"`
+    );
+    res.setHeader("Content-Length", file.length);
+
+    return res.send(file);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error al recuperar el archivo",
+      data: null,
+    });
+  }
+});
+
 
 export default patientFilesRouter
--- services/patient_files_services.js
+++ services/patient_files_services.js
@@ -6,3 +6,14 @@ export const addPatientFile = async (patientId, file, db) => {
         ($1, $2)
     `, [patientId, file])[0]?? null;
 }
+
+export const getMainPatientFile = async (patientId, db) => {
+    return (await db.exec(`
+        SELECT
+            file
+        FROM "patient_file"
+        WHERE 
+            patient_id = $1 AND 
+            is_main = true
+    `, [patientId]))[0] ?? null;
+}
